d5ddaf04415049a9a28c5e4f4b8cf6f51eb71c2a
Hans-Peter Herzog <hherzog@sipwise.com>
2020-04-22 09:40:34 +0200

    TT#80058 Bootstrap new quasar project
    - Configure ESLint (Code style)
    - Configure Axios (HTTP requester)
    - Create Login component
    - Create login action
    
    Change-Id: I241cb969dc0ace6a863b93bd33853e6abe8a5070

---
+++ src/pages/Login.vue
@@ -0,0 +1,82 @@
+<template>
+	<q-page>
+		<div
+			class="row justify-center"
+		>
+			<q-form
+				class="col col-3 q-gutter-y-md"
+			>
+				<div>
+					{{ $t('login.title') }}
+				</div>
+				<q-input
+					v-model="username"
+					autocomplete="username"
+					dense
+					color="primary"
+					clearable
+					:label="$t('login.usernameLabel')"
+				>
+					<template v-slot:prepend>
+						<q-icon name="person" />
+					</template>
+				</q-input>
+				<q-input
+					v-model="password"
+					autocomplete="current-password"
+					type="password"
+					dense
+					color="primary"
+					clearable
+					:label="$t('login.passwordLabel')"
+				>
+					<template v-slot:prepend>
+						<q-icon name="lock" />
+					</template>
+				</q-input>
+				<div
+					class="row justify-end"
+				>
+					<q-btn
+						color="primary"
+						@click="login({username: username, password: password})"
+					>
+						{{ $t('login.signInActionLabel') }}
+					</q-btn>
+				</div>
+			</q-form>
+		</div>
+	</q-page>
+</template>
+
+<script>
+import {
+	QPage,
+	QInput,
+	QIcon,
+	QForm
+} from 'quasar'
+import {
+	mapActions
+} from 'vuex'
+export default {
+	name: 'Login',
+	components: {
+		QPage,
+		QInput,
+		QIcon,
+		QForm
+	},
+	data () {
+		return {
+			username: '',
+			password: ''
+		}
+	},
+	methods: {
+		...mapActions('user', [
+			'login'
+		])
+	}
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,32 @@
+const routes = [
+	{
+		path: '/',
+		component: () => import('layouts/MainLayout.vue'),
+		children: [
+			{
+				path: '',
+				component: () => import('pages/Index.vue')
+			}
+		]
+	},
+	{
+		path: '/login',
+		component: () => import('layouts/Login.vue'),
+		children: [
+			{
+				path: '',
+				component: () => import('pages/Login.vue')
+			}
+		]
+	}
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+	routes.push({
+		path: '*',
+		component: () => import('pages/Error404.vue')
+	})
+}
+
+export default routes
