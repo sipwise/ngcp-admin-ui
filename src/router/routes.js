9044e3f439b14e0dc03f2223f1edcd14b652d7c3
Hans-Peter Herzog <hherzog@sipwise.com>
2020-05-07 13:37:20 +0200

    TT#81159 As an Admin, I want to see a 404 page for none existing content
    Change-Id: Ib7d6c1a08a2d03a1dd55fb5a0b41685b5f2dfe84

--- src/router/routes.js
+++ src/router/routes.js
@@ -33,7 +33,13 @@ const routes = [
 if (process.env.MODE !== 'ssr') {
 	routes.push({
 		path: '*',
-		component: () => import('pages/Error404.vue')
+		component: () => import('layouts/MainLayout.vue'),
+		children: [
+			{
+				path: '',
+				component: () => import('pages/Error404.vue')
+			}
+		]
 	})
 }
 
