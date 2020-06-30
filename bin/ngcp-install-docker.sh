54677ea5accf7f7e86daa6212ca2ffdabd7c7913
Hans-Peter Herzog <hherzog@sipwise.com>
2020-06-30 07:41:10 +0200

    TT#84228 Run Quasar development server in NGCP and serve it over NGINX
    Change-Id: I1c403c8976dda4916ff6f6c582f533d37d4cc45a

---
+++ bin/ngcp-install-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+apt-get install --assume-yes \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install --assume-yes \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io
