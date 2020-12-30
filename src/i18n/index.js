a193ee153e83f93eabc56479980a4bd50e35c1ca
Sergii Leonenko <sleonenko@sipwise.com>
2020-12-30 22:08:24 +0200

    TT#106500 i18n - Fallback to default language even if key is present
    AC:
    Can fallback to default language even if key is present but empty in language file of the selected language
    Can fallback to dynamic variable based translations properly
    Change-Id: Ib7b8c7f87236f4fbeaa3826aed7b277fdb2ec23a

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,10 +6,21 @@ import it from './it.json'
 import ru from './ru.json'
 
 export default {
-	'en-us': enUs,
-	de,
-	es,
-	fr,
-	it,
-	ru
+	'en-us': patchKeysForFallback(enUs),
+	de: patchKeysForFallback(de),
+	es: patchKeysForFallback(es),
+	fr: patchKeysForFallback(fr),
+	it: patchKeysForFallback(it),
+	ru: patchKeysForFallback(ru)
+}
+
+function patchKeysForFallback (messages = {}) {
+	// Note: not translated keys may be defined as keys with empty string value. We should delete or replace such keys
+	// with null or undefined value to allow VueI18n language fallback mechanism works properly
+	Object.entries(messages).forEach(([key, value]) => {
+		if (typeof value === 'string' && value.trim() === '') {
+			messages[key] = undefined
+		}
+	})
+	return messages
 }
