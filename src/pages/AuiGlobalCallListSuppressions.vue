e806c07367f48ef136a6750561f72afb680fa83f
Carlo Venusino <cvenusino@sipwise.com>
2021-01-29 15:28:07 +0100

    TT#108174 CallListSuppressions: Table configuration
    Change-Id: I7250c19a426419fa6915ef509115b3526e3f44aa

--- src/pages/AuiGlobalCallListSuppressions.vue
+++ src/pages/AuiGlobalCallListSuppressions.vue
@@ -6,14 +6,15 @@
 			ref="table"
 			table-id="globalcalllistsuppressions"
 			row-key="id"
-			resource=""
-			resource-base-path=""
+			resource="calllistsuppression"
+			resource-base-path="calllistsuppression"
 			resource-type="ajax"
-			resource-alt=""
+			resource-alt="calllistsuppression/ajax"
 			:resource-singular="$t('global call list suppression')"
 			:resource-plural="$t('global call list suppressions')"
 			:title="$t('Global Call List Suppressions')"
 			:columns="columns"
+			:searchable="true"
 		>
 		</aui-data-table>
 	</q-page>
@@ -32,7 +33,50 @@ export default {
 	},
 	computed: {
 		columns () {
-			return []
+			return [
+				{
+					name: 'id',
+					label: this.$t('Id'),
+					field: 'id',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'domain',
+					label: this.$t('Domain'),
+					field: 'domain',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'direction',
+					label: this.$t('Direction'),
+					field: 'direction',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'pattern',
+					label: this.$t('Pattern'),
+					field: 'pattern',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'mode',
+					label: this.$t('Mode'),
+					field: 'mode',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'label',
+					label: this.$t('Label'),
+					field: 'label',
+					sortable: true,
+					align: 'left'
+				}
+			]
 		}
 	}
 }
