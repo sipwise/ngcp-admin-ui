2a7934ff11289c67ea47557b69c3ccd22965f6af
Carlo Venusino <cvenusino@sipwise.com>
2021-02-19 15:33:58 +0100

    TT#108172 BillingProfiles - Table configuration
    Change-Id: I228864e408057206986d311f001ca9f0f27739ea

--- src/pages/AuiBillingProfiles.vue
+++ src/pages/AuiBillingProfiles.vue
@@ -1,17 +1,81 @@
 <template>
 	<q-page
 		class="q-pa-md"
-	/>
+	>
+		<aui-data-table
+			ref="table"
+			table-id="billing"
+			row-key="id"
+			resource="billing"
+			resource-base-path="billing"
+			resource-type="ajax"
+			resource-alt="billing/ajax"
+			resource-singular=""
+			resource-plural=""
+			:title="$t('Billing Profiles')"
+			:columns="columns"
+		/>
+	</q-page>
 </template>
 
 <script>
+import AuiDataTable from 'components/AuiDataTable'
 export default {
 	name: 'AuiBillingProfiles',
-	components: {},
+	components: {
+		AuiDataTable
+	},
 	data () {
 		return {
 		}
 	},
-	computed: {}
+	computed: {
+		columns () {
+			return [
+				{
+					name: 'id',
+					label: this.$t('Id'),
+					field: 'id',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'name',
+					label: this.$t('Name'),
+					field: 'name',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'handle',
+					label: this.$t('Reseller'),
+					field: 'handle',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'prepaid',
+					label: this.$t('Prepaid'),
+					field: 'prepaid',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'package_cnt',
+					label: this.$t('Used (contracts)'),
+					field: 'package_cnt',
+					sortable: true,
+					align: 'left'
+				},
+				{
+					name: 'contract_cnt',
+					label: this.$t('Used (packages)'),
+					field: 'contract_cnt',
+					sortable: true,
+					align: 'left'
+				}
+			]
+		}
+	}
 }
 </script>
