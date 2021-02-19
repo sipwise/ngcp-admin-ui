1196e39674c1698cf2e6b131dc23385f5e28ca7e
Carlo Venusino <cvenusino@sipwise.com>
2021-02-19 16:07:55 +0100

    TT#108169 BillingProfiles - Global and row actions
    Change-Id: I2beeefc45c01afe390908fee696c02744faa90f2

--- src/pages/AuiBillingProfiles.vue
+++ src/pages/AuiBillingProfiles.vue
@@ -14,16 +14,108 @@
 			resource-plural=""
 			:title="$t('Billing Profiles')"
 			:columns="columns"
-		/>
+			:searchable="true"
+			:deletable="true"
+			:row-deletable="(row) => row.contract_cnt < 1"
+			:deletion-label="$t('Terminate')"
+			deletion-subject="id"
+			deletion-title-i18n-key="Terminate billing profile"
+			deletion-text-i18n-key="You are about to delete billing profile # {subject}"
+		>
+			<template
+				v-slot:actions="props"
+			>
+				<q-btn
+					class="q-mr-xs"
+					icon="add"
+					color="primary"
+					unelevated
+					:to="'/billing/create'"
+					:disable="props.loading"
+					:label="$t('Add')"
+				/>
+				<q-btn-dropdown
+					class="q-mr-xs"
+					icon="edit"
+					color="primary"
+					unelevated
+					split
+					size="md"
+					:to="'/billing/' + props.row.id + '/edit'"
+					:disable="props.loading || props.selected"
+					:label="$t('Edit')"
+				>
+					<q-list
+						class="bg-white"
+					>
+						<aui-popup-menu-item
+							icon="content_copy"
+							color="primary"
+							:to="'/billing/'+ props.row.id +'/duplicate'"
+							:disable="props.loading || props.selected"
+							:label="$t('Duplicate')"
+						/>
+						<aui-popup-menu-item
+							icon="shopping_cart"
+							color="primary"
+							:to="'/billing/'+ props.row.id +'/fees'"
+							:disable="props.loading || props.selected"
+							:label="$t('Fees')"
+						/>
+						<aui-popup-menu-item
+							icon="query_builder"
+							color="primary"
+							:to="'/billing/'+ props.row.id +'/peaktimes'"
+							:disable="props.loading || props.selected"
+							:label="$t('Off-Peaktimes')"
+						/>
+					</q-list>
+				</q-btn-dropdown>
+			</template>
+			<template
+				v-slot:row-more-menu="props"
+			>
+				<q-list
+					class="bg-white"
+				>
+					<aui-popup-menu-item
+						icon="edit"
+						color="primary"
+						:to="'/billing/'+ props.row.id +'/duplicate'"
+						:label="$t('Edit')"
+					/>
+					<aui-popup-menu-item
+						icon="content_copy"
+						color="primary"
+						:to="'/billing/'+ props.row.id +'/duplicate'"
+						:label="$t('Duplicate')"
+					/>
+					<aui-popup-menu-item
+						icon="shopping_cart"
+						color="primary"
+						:to="'/billing/'+ props.row.id +'/fees'"
+						:label="$t('Fees')"
+					/>
+					<aui-popup-menu-item
+						icon="query_builder"
+						color="primary"
+						:to="'/billing/'+ props.row.id +'/peaktimes'"
+						:label="$t('Off-Peaktimes')"
+					/>
+				</q-list>
+			</template>
+		</aui-data-table>
 	</q-page>
 </template>
 
 <script>
 import AuiDataTable from 'components/AuiDataTable'
+import AuiPopupMenuItem from 'components/AuiPopupMenuItem'
 export default {
 	name: 'AuiBillingProfiles',
 	components: {
-		AuiDataTable
+		AuiDataTable,
+		AuiPopupMenuItem
 	},
 	data () {
 		return {
