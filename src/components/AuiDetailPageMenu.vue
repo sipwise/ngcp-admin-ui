8a7c58f2d29110e2ec14789e7703210ede613439
Hans-Peter Herzog <hherzog@sipwise.com>
2021-09-09 15:58:49 +0200

    TT#140750 Reuse main menu items for rendering the detail page navigations
    Change-Id: Ibf7d5d052755fb37f081ada59bd8d4f6fbdb0e17

--- src/components/AuiDetailPageMenu.vue
+++ src/components/AuiDetailPageMenu.vue
@@ -1,66 +1,20 @@
 <template>
     <q-drawer
-        v-model="drawerVisible"
         behavior="desktop"
-        content-class="nested-drawer-content"
         side="left"
-        :mini="minimized"
+        :mini="false"
+        :width="280"
         show-if-above
-        @mouseover="(!pinned && autoMinimize) ? minimized = false : null"
-        @mouseout="(!pinned && autoMinimize) ? minimized = true : null"
     >
-        <q-list>
-            <q-item class="q-px-xs">
-                <q-item-section
-                    avatar
-                >
-                    <q-btn
-                        v-show="minimized"
-                        icon="fas fa-caret-right"
-                        color="grey-9"
-                        flat
-                        dense
-                        round
-                        @click="minimized = false"
-                    />
-                    <q-btn
-                        v-show="!minimized && !pinned"
-                        icon="fas fa-thumbtack"
-                        color="grey-9"
-                        flat
-                        dense
-                        round
-                        @click="pinned = true"
-                    />
-                </q-item-section>
-
-                <q-item-section
-                    v-show="!minimized"
-                />
-                <q-item-section
-                    v-show="!minimized"
-                    side
-                >
-                    <q-btn
-                        icon="fas fa-caret-left"
-                        color="grey-9"
-                        flat
-                        dense
-                        round
-                        @click="closeAndUnpin"
-                    />
-                </q-item-section>
-            </q-item>
-
-            <aui-popup-menu-item
+        <q-list
+            class="q-pl-md q-pt-md"
+        >
+            <aui-main-menu-item
                 v-for="(item, index) in items"
                 :key="index"
                 :label="item.label"
                 :icon="item.icon"
                 :to="item.to"
-                dense
-                icon-size="xs"
-                color="dark"
             />
         </q-list>
     </q-drawer>
@@ -68,23 +22,13 @@
 
 <script>
 
-import AuiPopupMenuItem from 'components/AuiPopupMenuItem'
 import { mapState } from 'vuex'
 import _ from 'lodash'
+import AuiMainMenuItem from 'components/AuiMainMenuItem'
 
 export default {
     name: 'AuiDetailPageMenu',
-    components: { AuiPopupMenuItem },
-    props: {
-    },
-    data () {
-        return {
-            drawerVisible: true,
-            minimized: false,
-            pinned: true,
-            autoMinimize: true
-        }
-    },
+    components: { AuiMainMenuItem },
     computed: {
         ...mapState('page', [
             'resourceObject'
@@ -112,31 +56,6 @@ export default {
                 return []
             }
         }
-    },
-    methods: {
-        closeAndUnpin () {
-            this.autoMinimize = false
-            this.minimized = true
-            this.pinned = false
-            setTimeout(() => { this.autoMinimize = true }, 100)
-        }
     }
 }
 </script>
-
-<style lang="sass" rel="stylesheet/sass" scoped>
-@import 'src/css/quasar.variables.sass'
-
-.q-drawer-container::v-deep .nested-drawer-content
-    background: $nested-drawer-bg !important
-
-.q-router-link--active::v-deep
-    font-weight: bold
-
-    .q-drawer--mini & .q-icon
-        padding: 5px
-        margin: -5px
-        border-radius: 32px
-        border: 1px dashed $primary
-
-</style>
