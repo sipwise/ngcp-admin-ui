f9ce5e7bac7c861750a594120a18417604d416a0
Sergii Leonenko <sleonenko@sipwise.com>
2021-12-21 03:24:20 +0200

    TT#153802 Fix hard coded data-cy attribute behavior
    Change-Id: Icd188ee851785ebf9541d072aa137d6ee5016822

--- src/boot/e2e-testing.js
+++ src/boot/e2e-testing.js
@@ -9,49 +9,59 @@ export default ({ app, Vue }) => {
      */
 
     Vue.mixin({
+        watch: {
+            dataCyKey () {
+                this.updateDataCyAttribute()
+            }
+        },
         mounted () {
-            if (this.$vnode && this.$el && this.$el.setAttribute && this.$el.getAttribute && !this.$el.getAttribute('data-cy')) {
-                // trying to get the component's attribute "data-cy"
-                const componentDataCyAttr = this.$vnode.componentInstance?.$attrs?.['data-cy']
-                let dataCy = ''
-                if (!componentDataCyAttr) {
-                    // Trying to pick a parent wrapper component's name. Like the name of our custom component which contains some "q-..." component
-                    const getNestedComponentsNames = ($vnode) => {
-                        if (!$vnode) {
-                            return []
+            this.updateDataCyAttribute()
+        },
+        methods: {
+            updateDataCyAttribute () {
+                if (this.$vnode && this.$el && this.$el.setAttribute) {
+                    // trying to get the component's attribute "data-cy"
+                    const componentDataCyAttr = this.$vnode.componentInstance?.$attrs?.['data-cy']
+                    let dataCy = ''
+                    if (!componentDataCyAttr) {
+                        // Trying to pick a parent wrapper component's name. Like the name of our custom component which contains some "q-..." component
+                        const getNestedComponentsNames = ($vnode) => {
+                            if (!$vnode) {
+                                return []
+                            }
+                            const name = _.get($vnode, 'componentOptions.Ctor.extendOptions.name', null)
+                            const key = ($vnode.key !== undefined && !String($vnode.key).startsWith('__transition')) ? $vnode.key : null
+
+                            const parentData = ($vnode.elm === $vnode?.parent?.elm) ? getNestedComponentsNames($vnode.parent) : []
+                            return [[name, key, $vnode], ...parentData]
                         }
-                        const name = _.get($vnode, 'componentOptions.Ctor.extendOptions.name', null)
-                        const key = ($vnode.key !== undefined && !String($vnode.key).startsWith('__transition')) ? $vnode.key : null
+                        const componentNames = getNestedComponentsNames(this.$vnode)
 
-                        const parentData = ($vnode.elm === $vnode?.parent?.elm) ? getNestedComponentsNames($vnode.parent) : []
-                        return [[name, key, $vnode], ...parentData]
-                    }
-                    const componentNames = getNestedComponentsNames(this.$vnode)
-
-                    // let's skip all "transition" root components if it is possible
-                    let finalItem
-                    while (componentNames.length > 0 && !finalItem) {
-                        const currentItem = componentNames.pop()
-                        const name = currentItem[0]
-                        if (name !== 'transition' || componentNames.length === 0) {
-                            finalItem = currentItem
+                        // let's skip all "transition" root components if it is possible
+                        let finalItem
+                        while (componentNames.length > 0 && !finalItem) {
+                            const currentItem = componentNames.pop()
+                            const name = currentItem[0]
+                            if (name !== 'transition' || componentNames.length === 0) {
+                                finalItem = currentItem
+                            }
                         }
+                        const [componentName, instanceKey, f$vnode] = finalItem || []
+
+                        // trying to read component's computed property "dataCyKey"
+                        const customInstanceKey = f$vnode?.componentInstance?.dataCyKey
+                        const dataCyKey = (customInstanceKey !== undefined && customInstanceKey !== null) ? customInstanceKey : instanceKey
+
+                        dataCy = _.kebabCase(componentName) + ((dataCyKey !== null) ? '--' + _.kebabCase(dataCyKey) : '')
+                    } else {
+                        dataCy = componentDataCyAttr
                     }
-                    const [componentName, instanceKey, f$vnode] = finalItem || []
 
-                    // trying to read component's computed property "dataCyKey"
-                    const customInstanceKey = f$vnode?.componentInstance?.dataCyKey
-                    const dataCyKey = (customInstanceKey !== undefined && customInstanceKey !== null) ? customInstanceKey : instanceKey
-
-                    dataCy = _.kebabCase(componentName) + ((dataCyKey !== null) ? '--' + _.kebabCase(dataCyKey) : '')
-                } else {
-                    dataCy = componentDataCyAttr
-                }
-
-                if (dataCy !== '') {
-                    this.$el.setAttribute('data-cy', dataCy)
-                } else if (process.env.DEV) {
-                    console.warn('data-cy generation failed due to missing component name', this.$el)
+                    if (dataCy !== '') {
+                        this.$el.setAttribute('data-cy', dataCy)
+                    } else if (process.env.DEV) {
+                        console.warn('data-cy generation failed due to missing component name', this.$el)
+                    }
                 }
             }
         }
