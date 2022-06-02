5d39f8fe15dfdee1c00c64944e0a722d19abd2da
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2022-06-02 11:56:58 +0200

    TT#163102 BillingProfiles - Migrate main list page
    Change-Id: I9f36eaf8b6ccde36517b312f64e3786cd4492b78

---
+++ src/pages/billing-profiles/AuiBillingProfilesList.vue
@@ -0,0 +1,129 @@
+<template>
+    <aui-base-list-page
+        @refresh="refresh"
+    >
+        <aui-data-table
+            ref="dataTable"
+            table-id="billingprofiles"
+            resource="billingprofiles"
+            resource-base-path="billingprofile"
+            resource-search-field="name"
+            :resource-search-wildcard="true"
+            resource-type="ajax"
+            resource-alt="billing/ajax"
+            :resource-singular="$t('Billing Profiles')"
+            :resource-default-filters="{ profile_set_id: profileSetId }"
+            row-key="id"
+            :title="$t('Billing Profiles')"
+            :columns="columns"
+            :searchable="true"
+            :editable="true"
+            :row-editable="() => true"
+            :addable="true"
+            :deletable="true"
+            deletion-subject="id"
+            :show-header="false"
+            :add-action-routes="[{ name: 'billingProfileCreation'}]"
+            :row-actions="() => {
+                return [
+                    'billingProfileEdit',
+                    'billingProfileDuplicate',
+                    'billingProfileFees',
+                    'billingProfilePeaktimes',
+                    'billingProfilesJournal'
+                ]}"
+            :search-criteria-config="[
+                {
+                    criteria: 'name',
+                    label: $t('Name'),
+                    component: 'input'
+                }
+            ]"
+        />
+    </aui-base-list-page>
+</template>
+
+<script>
+import AuiDataTable from 'components/AuiDataTable'
+import AuiBaseListPage from 'pages/AuiBaseListPage'
+import dataTable from 'src/mixins/data-table'
+import { required } from 'vuelidate/lib/validators'
+export default {
+    name: 'AuiBillingProfilesList',
+    components: {
+        AuiBaseListPage,
+        AuiDataTable
+    },
+    mixins: [
+        dataTable
+    ],
+    props: {
+        profileSetId: {
+            type: Number,
+            default: null
+        }
+    },
+    computed: {
+        columns () {
+            return [
+                {
+                    name: 'id',
+                    label: this.$t('Id'),
+                    field: 'id',
+                    sortable: true,
+                    align: 'center'
+                },
+                {
+                    name: 'name',
+                    label: this.$t('Name'),
+                    field: 'name',
+                    sortable: true,
+                    editable: true,
+                    align: 'left',
+                    component: 'input',
+                    componentValidations: [
+                        {
+                            name: 'required',
+                            validator: required,
+                            error: this.$t('Name must not be empty')
+                        }
+                    ]
+                },
+                {
+                    name: 'reseller',
+                    label: this.$t('Reseller'),
+                    field: 'handle',
+                    sortable: true,
+                    align: 'left',
+                    editable: false
+                },
+                {
+                    name: 'prepaid',
+                    label: this.$t('Prepaid'),
+                    field: 'prepaid',
+                    sortable: true,
+                    align: 'left',
+                    editable: true,
+                    component: 'toggle'
+                },
+                {
+                    name: 'used_contracts',
+                    label: this.$t('Used (contracts)'),
+                    field: 'contract_cnt',
+                    sortable: true,
+                    align: 'left',
+                    editable: false
+                },
+                {
+                    name: 'used_packages',
+                    label: this.$t('Used (packages)'),
+                    field: 'package_cnt',
+                    sortable: true,
+                    align: 'left',
+                    editable: false
+                }
+            ]
+        }
+    }
+}
+</script>
