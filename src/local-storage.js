86fcaa6bc479098aa987cba863f54b5b0849ff86
Hans-Peter Herzog <hherzog@sipwise.com>
2022-06-09 14:52:30 +0200

    TT#163500 After login v2 opens dashboard while v1 is opening the last context/page
    Change-Id: I62b4dfb2524a86e35ff65fd4c1512c1ad76f6e61

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,16 +1,31 @@
 
 import {
-    LocalStorage
+    LocalStorage,
+    SessionStorage
 } from 'quasar'
 
+const VARIABLE_PREFIX = 'aui_'
+
 export function setLocal (name, value) {
-    LocalStorage.set('aui_' + name, value)
+    LocalStorage.set(VARIABLE_PREFIX + name, value)
 }
 
 export function getLocal (name) {
-    return LocalStorage.getItem('aui_' + name)
+    return LocalStorage.getItem(VARIABLE_PREFIX + name)
 }
 
 export function deleteLocal (name) {
-    LocalStorage.remove('aui_' + name)
+    LocalStorage.remove(VARIABLE_PREFIX + name)
+}
+
+export function setSessionStorage (name, value) {
+    SessionStorage.set(VARIABLE_PREFIX + name, value)
+}
+
+export function getSessionStorage (name) {
+    return SessionStorage.getItem(VARIABLE_PREFIX + name)
+}
+
+export function delSessionStorage (name) {
+    SessionStorage.remove(VARIABLE_PREFIX + name)
 }
