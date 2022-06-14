2340668b35de12c36e74c0b1a2d980f494941ee9
Hugo Zigha <hugo.zigha@al-enterprise.com>
2022-06-14 14:44:39 +0200

    TT#163101 BillingProfiles - Migrate add page
    Change-Id: I03d9913eca1520a064c276d01d1089130bcfffb7

--- src/pages/billing-profiles/AuiBillingProfileCreation.vue
+++ src/pages/billing-profiles/AuiBillingProfileCreation.vue
@@ -1,13 +1,50 @@
 <template>
-    <aui-base-page>
-        <proxy />
-    </aui-base-page>
+    <aui-base-add-page>
+        <template
+            #default="{ initialFormData }"
+        >
+            <aui-new-billing-profile
+                :initial-form-data="initialFormData"
+                :loading="$waitPage()"
+                @submit="create"
+            >
+                <template
+                    #actions="{ loading, hasInvalidData, submit }"
+                >
+                    <aui-form-actions-creation
+                        :loading="loading"
+                        :has-invalid-data="hasInvalidData"
+                        @submit="submit"
+                    />
+                </template>
+            </aui-new-billing-profile>
+        </template>
+    </aui-base-add-page>
 </template>
 
 <script>
-import Proxy from 'pages/Proxy'
-import AuiBasePage from 'pages/AuiBasePage'
+import AuiFormActionsCreation from 'components/AuiFormActionsCreation'
+import { showGlobalSuccessMessage } from 'src/helpers/ui'
+import { mapWaitingActions } from 'vue-wait'
+import { WAIT_PAGE } from 'src/constants'
+import AuiBaseAddPage from 'pages/AuiBaseAddPage'
+import AuiNewBillingProfile from 'components/edit-forms/AuiNewBillingProfile'
 export default {
-    components: { AuiBasePage, Proxy }
+    name: 'AuiBillingProfileCreation',
+    components: {
+        AuiNewBillingProfile,
+        AuiBaseAddPage,
+        AuiFormActionsCreation
+    },
+    methods: {
+        ...mapWaitingActions('billing', {
+            createBillingProfile: WAIT_PAGE
+        }),
+        async create (data) {
+            await this.createBillingProfile(data)
+            await this.$auiGoToPrevForm()
+            showGlobalSuccessMessage(this.$t('Billing profile created successfully'))
+        }
+    }
 }
 </script>
