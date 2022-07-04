a53fbe077e5e82bb0b890c540bf901aa3749f5e4
Hans-Peter Herzog <hherzog@sipwise.com>
2022-07-04 18:13:00 +0200

    TT#183950 SubscriberDetails - Re-organize MasterData page
    Change-Id: I00a424b7c0324743067ad949be10727bb2d27e93

---
+++ src/pages/subscriber-details/AuiMasterDataItemBoolean.vue
@@ -0,0 +1,26 @@
+<template>
+    <q-item>
+        <q-item-section>
+            <q-toggle
+                :value="value"
+                :label="label"
+                disable
+            />
+        </q-item-section>
+    </q-item>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
---
+++ src/pages/subscriber-details/AuiMasterDataNumberList.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-item>
+        <q-item-section>
+            <q-item-label
+                caption
+            >
+                {{ label }}
+            </q-item-label>
+            <q-item-label
+                v-for="(number, index) in value"
+                :key="index"
+            >
+                {{ number | formatPhoneNumber }}
+                <q-icon
+                    v-if="number.is_devid"
+                    class="q-ml-xs"
+                    color="default"
+                    name="phone"
+                    size="xs"
+                />
+            </q-item-label>
+            <q-item-label
+                v-if="!value || (value && value.length === 0)"
+            >
+                {{ $t('N/A') }}
+            </q-item-label>
+        </q-item-section>
+    </q-item>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
