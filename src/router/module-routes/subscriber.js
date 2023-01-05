fdccd9011ead5c20aed55acf9b3e9c127aa014e9
Marco Capetta <mcapetta@sipwise.com>
2023-01-05 15:56:21 +0100

    MT#54829 Remove Fax and Header Manipulation menu from CE
    Those are not CE features, so they should be hidden
    from the Subscriber's Details page.
    
    Change-Id: Id2dc6734c4d18024b8d722a00a7c88413759dc20
    (cherry picked from commit 04d0c46e897598b90577d9bf357cb1343919c222)

--- src/router/module-routes/subscriber.js
+++ src/router/module-routes/subscriber.js
@@ -374,7 +374,8 @@ export default [
                             parentPath: 'subscriberList.subscriberContext.subscriberDetails',
                             icon: 'fax',
                             v1DetailsPageSectionId: 'collapse_fax',
-                            goToPathRewrite: detailsPageToPreferencesPagePathRewrite
+                            goToPathRewrite: detailsPageToPreferencesPagePathRewrite,
+                            platformVersions: [PLATFORM_PRO, PLATFORM_CARRIER]
                         }
                     },
                     {
@@ -631,7 +632,8 @@ export default [
                             parentPath: 'subscriberList.subscriberContext.subscriberDetails',
                             icon: 'fas fa-edit',
                             v1DetailsPageSectionId: 'collapse_hdr',
-                            goToPathRewrite: detailsPageToPreferencesPagePathRewrite
+                            goToPathRewrite: detailsPageToPreferencesPagePathRewrite,
+                            platformVersions: [PLATFORM_PRO, PLATFORM_CARRIER]
                         }
                     },
                     {
