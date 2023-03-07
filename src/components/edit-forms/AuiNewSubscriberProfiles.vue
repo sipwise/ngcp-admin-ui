47b2902630bcdc1c81617327570f3c273ddeb05c
Hugo Zigha <hugo.zigha@al-enterprise.com>
2023-03-07 18:37:23 +0100

    MT#56403 Improve subscriber's profiles edit page
    The page has been re-organized and all the attributes
    are not grouped in sections with dedicated FLIP and
    TOGGLE buttons
    
    Change-Id: I7509c5d7d70f7cd815e0e48e39c489982da52072

--- src/components/edit-forms/AuiNewSubscriberProfiles.vue
+++ src/components/edit-forms/AuiNewSubscriberProfiles.vue
@@ -108,21 +108,57 @@
                     @click="flipAllFlags"
                 />
             </q-item-label>
-            <q-item
-                v-for="(item, index) of attributesList"
-                :key="index"
-                class="col-lg-3 col-md-4 col-sm-12 col-xs-12"
+            <template
+                v-for="(category, indexCategory) of categoryList"
             >
-                <q-item-section>
-                    <q-toggle
-                        :value="isChecked(item)"
-                        dense
-                        :label="item"
-                        :disable="loading"
-                        @input="toggleChecked(item, index)"
+                <q-item-label
+                    :key="indexCategory"
+                    header
+                    class="text-uppercase col-12"
+                >
+                    <q-icon
+                        name="list"
+                        size="sm"
+                        class="q-mr-sm"
                     />
-                </q-item-section>
-            </q-item>
+                    {{ category.label }}
+                    <q-btn
+                        class="q-ml-md"
+                        icon="fas fa-toggle-on"
+                        color="primary"
+                        size="sm"
+                        :label="$t('Toggle on/off')"
+                        unelevated
+                        outline
+                        @click="toggleOnOffCategory(category.value)"
+                    />
+                    <q-btn
+                        class="q-ml-md"
+                        icon="exposure"
+                        color="primary"
+                        size="sm"
+                        :label="$t('Flip')"
+                        unelevated
+                        outline
+                        @click="flipCategory(category.value)"
+                    />
+                </q-item-label>
+                <q-item
+                    v-for="(item, index) of attributesListCategory(category.value)"
+                    :key="indexCategory + '-' + index"
+                    class="col-lg-3 col-md-4 col-sm-12 col-xs-12"
+                >
+                    <q-item-section>
+                        <q-toggle
+                            :value="isChecked(item.value)"
+                            dense
+                            :label="item.value"
+                            :disable="loading"
+                            @input="toggleChecked(item.value, index)"
+                        />
+                    </q-item-section>
+                </q-item>
+            </template>
         </q-list>
     </aui-base-form>
 </template>
@@ -167,56 +203,93 @@ export default {
         // TODO replace hardcoded attributes with endpoint when available
         attributesList () {
             return [
-                'block_in_mode',
-                'block_in_list',
-                'block_in_clir',
-                'block_out_mode',
-                'block_out_list',
-                'cfu',
-                'cfb',
-                'cfna',
-                'cft',
-                'clir',
-                'ncos',
-                'block_out_override_pin',
-                'contract_sound_set',
-                'music_on_hold',
-                'clir_intrapbx',
-                'speed_dial',
-                'reminder',
-                'auto_attendant',
-                'voice_mail',
-                'fax_server',
-                'cloud_pbx_callqueue',
-                'language',
-                'conference_pin',
-                'max_queue_length',
-                'queue_wrap_up_time',
-                'conference_max_participants',
-                'clir_override',
-                'codecs_filter',
-                'codecs_list',
-                'cfs',
-                'secretary_numbers',
-                'manager_secretary',
-                'csta_controller',
-                'csta_client',
-                'advice_of_charge',
-                'accept_auto_answer',
-                'rerouting_mode',
-                'rerouting_codes',
-                'cfr',
-                'codecs_id_filter',
-                'codecs_id_list',
-                'colp_cf',
-                'cfo',
-                'csc_calls',
-                'play_announce_before_cf',
-                'play_announce_before_call_setup',
-                'play_announce_before_recording',
-                'play_announce_to_callee',
-                'contact_ringtimeout'
+                { value: 'block_in_mode', category: 'block' },
+                { value: 'block_in_list', category: 'block' },
+                { value: 'block_in_clir', category: 'block' },
+                { value: 'block_out_mode', category: 'block' },
+                { value: 'block_out_list', category: 'block' },
+                { value: 'block_out_override_pin', category: 'block' },
+                { value: 'ncos', category: 'block' },
+                { value: 'cfu', category: 'forward' },
+                { value: 'cfb', category: 'forward' },
+                { value: 'cfna', category: 'forward' },
+                { value: 'cft', category: 'forward' },
+                { value: 'cfs', category: 'forward' },
+                { value: 'cfo', category: 'forward' },
+                { value: 'cfr', category: 'forward' },
+                { value: 'rerouting_mode', category: 'forward' },
+                { value: 'rerouting_codes', category: 'forward' },
+                { value: 'music_on_hold', category: 'media' },
+                { value: 'contract_sound_set', category: 'media' },
+                { value: 'play_announce_before_cf', category: 'media' },
+                { value: 'play_announce_before_call_setup', category: 'media' },
+                { value: 'play_announce_before_recording', category: 'media' },
+                { value: 'play_announce_to_callee', category: 'media' },
+                { value: 'speed_dial', category: 'features' },
+                { value: 'reminder', category: 'features' },
+                { value: 'voice_mail', category: 'features' },
+                { value: 'fax_server', category: 'features' },
+                { value: 'conference_pin', category: 'features' },
+                { value: 'conference_max_participants', category: 'features' },
+                { value: 'record_call', category: 'features' },
+                { value: 'advice_of_charge', category: 'features' },
+                { value: 'secretary_numbers', category: 'pbxFeatures' },
+                { value: 'manager_secretary', category: 'pbxFeatures' },
+                { value: 'max_queue_length', category: 'pbxFeatures' },
+                { value: 'cloud_pbx_callqueue', category: 'pbxFeatures' },
+                { value: 'queue_wrap_up_time', category: 'pbxFeatures' },
+                { value: 'auto_attendant', category: 'pbxFeatures' },
+                { value: 'csta_controller', category: 'pbxFeatures' },
+                { value: 'csta_client', category: 'pbxFeatures' },
+                { value: 'csc_calls', category: 'csc' },
+                { value: 'csc_device_provisioning', category: 'csc' },
+                { value: 'csc_conversations', category: 'csc' },
+                { value: 'csc_registered_devices', category: 'csc' },
+                { value: 'csc_hunt_groups', category: 'csc' },
+                { value: 'language', category: 'general' },
+                { value: 'clir', category: 'general' },
+                { value: 'clir_intrapbx', category: 'general' },
+                { value: 'clir_override', category: 'general' },
+                { value: 'colp_cf', category: 'general' },
+                { value: 'contact_ringtimeout', category: 'general' },
+                { value: 'accept_auto_answer', category: 'general' },
+                { value: 'codecs_filter', category: 'general' },
+                { value: 'codecs_list', category: 'general' },
+                { value: 'codecs_id_filter', category: 'general' },
+                { value: 'codecs_id_list', category: 'general' }
             ].sort()
+        },
+        categoryList () {
+            return [
+                {
+                    label: this.$t('Call blocking profile flags'),
+                    value: 'block'
+                },
+                {
+                    label: this.$t('Call forwarding profile flags'),
+                    value: 'forward'
+                },
+                {
+                    label: this.$t('Media profile flags'),
+                    value: 'media'
+                },
+                {
+                    label: this.$t('Features profile flags'),
+                    value: 'features'
+                },
+                {
+                    label: this.$t('PBX Features profile flags'),
+                    value: 'pbxFeatures'
+                },
+                {
+                    label: this.$t('CSC sections profile flags'),
+                    value: 'csc'
+                },
+                {
+                    label: this.$t('General profile flags'),
+                    value: 'general'
+                }
+            ]
         },
         getDefaultData () {
             return {
@@ -238,16 +311,42 @@ export default {
                 : this.formData.attributes.push(attribute)
         },
         toggleOnOffAllFlags () {
-            if (this.attributesList.every(attribute => this.isChecked(attribute))) {
+            if (this.attributesList.every(attribute => this.isChecked(attribute.value))) {
                 this.formData.attributes = []
             } else {
-                this.formData.attributes = [...this.attributesList]
+                this.formData.attributes = Array.from(this.attributesList, attribute => attribute.value)
             }
         },
         flipAllFlags () {
             this.formData.attributes = this.attributesList.reduce((acc, attribute) => {
-                if (!this.isChecked(attribute)) {
-                    acc.push(attribute)
+                if (!this.isChecked(attribute.value)) {
+                    acc.push(attribute.value)
+                }
+                return acc
+            }, [])
+        },
+        attributesListCategory (category) {
+            return this.attributesList.filter((attribute) => attribute.category === category).sort()
+        },
+        toggleOnOffCategory (category) {
+            const attributeListCategory = this.attributesListCategory(category)
+            if (attributeListCategory.every(attribute => this.isChecked(attribute.value))) {
+                this.formData.attributes = this.formData.attributes.filter((attribute) => !attributeListCategory.some(attributeObject => attributeObject.value === attribute))
+            } else {
+                attributeListCategory.map((attribute) => {
+                    if (!this.formData.attributes.includes(attribute.value)) {
+                        this.formData.attributes.push(attribute.value)
+                    }
+                })
+            }
+        },
+        flipCategory (category) {
+            this.formData.attributes = this.attributesList.reduce((acc, attribute) => {
+                if (!this.isChecked(attribute.value) && attribute.category === category) {
+                    acc.push(attribute.value)
+                }
+                if (this.isChecked(attribute.value) && attribute.category !== category) {
+                    acc.push(attribute.value)
                 }
                 return acc
             }, [])
