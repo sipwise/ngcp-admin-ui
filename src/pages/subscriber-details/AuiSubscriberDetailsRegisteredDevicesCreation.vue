5b48fab8af2a7c61ad10a8ccf52d571c69da3f2f
Marco Capetta <mcapetta@sipwise.com>
2024-02-13 11:45:22 +0100

    MT#59387 Disable nat flag when creating a permanent registration
    In the OLD panel all the permanent registration were created without
    the nat flag and the keepalive value set, in the NEW panel instead
    we are doing the opposite. This commit fix this issue.
    
    In the future we can think to dinamically read the value of the
    'nat' preference and set the proper value for the permanent
    registration.
    
    Change-Id: I707f2ccf97db51dfa320797fe7d35738be94bed0

--- src/pages/subscriber-details/AuiSubscriberDetailsRegisteredDevicesCreation.vue
+++ src/pages/subscriber-details/AuiSubscriberDetailsRegisteredDevicesCreation.vue
@@ -60,7 +60,7 @@ export default {
         async create (data) {
             data.subscriber_id = this.subscriberId
             data.expires = '2099-01-01 00:00:00' // Hardcoded future date
-            data.nat = true
+            data.nat = false
             await this.createRegisteredDevice(data)
             showGlobalSuccessMessage(this.$t('Registered Device created successfully'))
             await this.$auiGoToPrevForm()
