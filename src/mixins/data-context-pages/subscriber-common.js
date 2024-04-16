7b2c1d6847ff44ff48a4418fcc106f630835b134
Nouhaila Idrissi Zouggari <nouhaila.idrissi-zouggari@al-enterprise.com>
2024-04-16 15:20:12 +0200

    MT#59791 Aliases under Pilot user are not listed or cannot be used by PBX Group
    Change-Id: I99e0cb357970fdae0c95f9cf7821203287e005aa
    (cherry picked from commit 39372fa269466a8b5d1d2ad37a03554e7013f7c9)

--- src/mixins/data-context-pages/subscriber-common.js
+++ src/mixins/data-context-pages/subscriber-common.js
@@ -2,40 +2,6 @@ import { mapWaitingActions } from 'vue-wait'
 import { WAIT_PAGE } from 'src/constants'
 
 export default {
-    computed: {
-        subscriberCommonPbxHuntPolicyOptions () {
-            return [
-                {
-                    label: this.$t('Serial Ringing'),
-                    value: 'serial'
-                },
-                {
-                    label: this.$t('Parallel Ringing'),
-                    value: 'parallel'
-                },
-                {
-                    label: this.$t('Random Ringing'),
-                    value: 'random'
-                },
-                {
-                    label: this.$t('Circular Ringing'),
-                    value: 'circular'
-                }
-            ]
-        },
-        subscriberCommonPbxHuntCancelModeOptions () {
-            return [
-                {
-                    label: this.$t('Using Cancel'),
-                    value: 'cancel'
-                },
-                {
-                    label: this.$t('Using Bye'),
-                    value: 'bye'
-                }
-            ]
-        }
-    },
     methods: {
         ...mapWaitingActions('customers', {
             assignNumbersToSubscriber: WAIT_PAGE,
--- src/store/customers/getters.js
+++ src/store/customers/getters.js
@@ -20,3 +20,45 @@ export function customerStatusOptions () {
         }
     ]
 }
+export function filteredCustomersPbxOptions (state) {
+    const customers = []
+    state.customersPbx.forEach((customer) => {
+        customers.push({
+            label: customer.id + ' - ' + customer.contact_email,
+            value: customer.id
+        })
+    })
+    return customers
+}
+export function subscriberCommonPbxHuntPolicyOptions () {
+    return [
+        {
+            label: i18n.global.tc('Serial Ringing'),
+            value: 'serial'
+        },
+        {
+            label: i18n.global.tc('Parallel Ringing'),
+            value: 'parallel'
+        },
+        {
+            label: i18n.global.tc('Random Ringing'),
+            value: 'random'
+        },
+        {
+            label: i18n.global.tc('Circular Ringing'),
+            value: 'circular'
+        }
+    ]
+}
+export function subscriberCommonPbxHuntCancelModeOptions () {
+    return [
+        {
+            label: i18n.global.tc('Using Cancel'),
+            value: 'cancel'
+        },
+        {
+            label: i18n.global.tc('Using Bye'),
+            value: 'bye'
+        }
+    ]
+}
