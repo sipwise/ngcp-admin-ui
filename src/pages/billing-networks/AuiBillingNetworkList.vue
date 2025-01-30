b9534d43189ce231a2878c3a304e2e1f88e1f720
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-01-30 09:46:24 +0000

    MT#61981 Review journal feature app-wide
    Currently the journal can be enabled
    only for those entity listed in the
    config.yml (see api_journal).
    This commit removes the journal menu
    from the UI for those entities that
    are not listed.
    Note: ncos journal is active because
    we are using API V2
    
    Change-Id: I8bc98f372c60e647d4269f9936088e51c02f52a9
    (cherry picked from commit d075429b4551a8cba8510aea2888fbb87472073f)

--- src/pages/billing-networks/AuiBillingNetworkList.vue
+++ src/pages/billing-networks/AuiBillingNetworkList.vue
@@ -27,7 +27,6 @@
             :row-actions="() => {
                 return [
                     'billingNetworkEdit',
-                    'billingNetworkJournal'
                 ]
             }"
         />
