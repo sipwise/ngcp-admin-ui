1e4eb750ca9ace7668c16349468c0d9e35e9ad31
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-01-31 15:29:18 +0000

    MT#62044 Display newest CF forms first
    * Display new CF forms at the top of the list
    * Remove unused css code
    * Show "add new destination" button at the
      top of the list
    
    Change-Id: I2e59b4cbf7dcd05eb3d49eb27655320b9d879193
    (cherry picked from commit a695ca4fe1c12caa2dc5147ae584edd447646029)
    (cherry picked from commit 7d894d4ae697e20cc72510e2445561011bd025e9)

--- src/components/edit-forms/AuiNewCallForwardingUnconditional.vue
+++ src/components/edit-forms/AuiNewCallForwardingUnconditional.vue
@@ -13,8 +13,21 @@
         />
         <div
             v-if="formData?.cfu?.length > 0"
-            class="flex-container"
+            class="text-center"
         >
+            <q-btn
+                :label="$t('Add Destination/time/BNumber/Source sets')"
+                data-cy="aui-cfunconditional-addnew-set"
+                class="q-mb-md"
+                color="primary"
+                icon="add"
+                size="sm"
+                unelevated
+                outline
+                :disable="loading"
+                @click="addCFU"
+            />
+
             <div
                 v-for="(cfu, index) in formData.cfu"
                 :key="index"
@@ -148,18 +161,6 @@
                     </q-card-actions>
                 </q-card>
             </div>
-
-            <q-btn
-                :label="$t('Add Destination/time/BNumber/Source sets')"
-                data-cy="aui-cfunconditional-addnew-set"
-                color="primary"
-                icon="add"
-                size="sm"
-                unelevated
-                outline
-                :disable="loading"
-                @click="addCFU"
-            />
         </div>
     </aui-base-form>
 </template>
@@ -272,7 +273,7 @@ export default {
     },
     methods: {
         addCFU () {
-            this.formData.cfu.push({
+            this.formData.cfu.unshift({
                 enabled: true,
                 use_redirection: false,
                 destinationset_id: null,
@@ -299,12 +300,7 @@ export default {
 }
 </script>
 <style>
-.green-border {
-    border: 1px solid green;
-    padding: 2%;
-    margin: 1%;
-    border-radius: 7px;
-}
+
 .list-container {
     display: flex;
     align-items: center;
