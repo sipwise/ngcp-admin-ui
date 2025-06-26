8e738937072d3afc2278b6eda4a8b8b0caf829df
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-06-26 15:53:10 +0100

    MT#63059 Reseller Billing Profile Details
    The component functionality was broken
    when attempting to hide reseller select
    for the scenario where the route was
    accessed under the reseller details.
    This commit:
    1.Introduces a new logic to
    decide wether the reseller select is shown:
    * Added `getHideResellerSelect()` computed
      property that checks if the component is
      accessed from a reseller details page by
      checking the route path includes
    * `created()` logic to automatically
      assign the reseller_id when we are in the
      reseller details scenario
    2.Fixed a typo in the component name
      'AuiResellerDetailsBillingProfileCreation'
    3.Made clear that in `rowActionRouteIntercept`
      `route.params.id` is being set to the
      reseller_id. See my comment about the
      dual meaning of the 'id' parameter in
      different scenarios.
    
    Change-Id: Ie41ec4d5b58ee5d825ca5c1e1e78741788faa571
    (cherry picked from commit 606edfb6b3da69be704206f4b8f1573db4bf623e)

--- src/pages/reseller-details/AuiResellerDetailsBillingProfileCreation.vue
+++ src/pages/reseller-details/AuiResellerDetailsBillingProfileCreation.vue
@@ -31,7 +31,7 @@ import { WAIT_PAGE } from 'src/constants'
 import { showGlobalSuccessMessage } from 'src/helpers/ui'
 import { mapWaitingActions } from 'vue-wait'
 export default {
-    name: 'AuiResellerDeatilsBillingProfileCreation',
+    name: 'AuiResellerDetailsBillingProfileCreation',
     components: {
         AuiBaseSubContext,
         AuiFormActionsCreation,
