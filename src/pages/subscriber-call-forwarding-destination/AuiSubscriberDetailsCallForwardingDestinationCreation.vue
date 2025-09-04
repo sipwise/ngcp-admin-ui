bce50d8ced3b76787e18414b5b1426686943cbc2
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-09-04 09:32:19 +0100

    MT#63512 Destinations list bugfix
    hasCapabilities() checks the logged-in user's capabilities.
    In this case, we only need to know if the subscriber is PBX to
    filter destination items accordingly. Instead of using
    hasCapabilities(), we call subscriberContextCustomerIsPbx()
    from the subscriber context.
    
    Change-Id: Iebad7090ce902548e46f27404c542af0b60fcaf1
    (cherry picked from commit 4f6a51d9cfd093411b94498ff90c87439e232e7e)

--- src/pages/subscriber-call-forwarding-destination/AuiSubscriberDetailsCallForwardingDestinationCreation.vue
+++ src/pages/subscriber-call-forwarding-destination/AuiSubscriberDetailsCallForwardingDestinationCreation.vue
@@ -9,6 +9,7 @@
                 :loading="$waitPage($wait)"
                 :subscriber-id="subscriberContext.id"
                 :primary-number-object="subscriberContext.primary_number"
+                :is-pbx-account="subscriberContextCustomerIsPbx"
                 @submit="create"
             >
                 <template
