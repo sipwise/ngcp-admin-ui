b02a1af9aa2fd47157618eab7f6f5675bb97d995
nidrissi-zouggari <nouhaila.idrissi-zouggari@al-enterprise.com>
2025-12-01 13:51:52 +0100

    MT#64019 Axios errors when entering pbx device edit page
    Cypress tests were failing when accessing the PBX device editing page.
    The issue occurred because the form was using profile_id in the model
    dropdown selector, when it should have been using device profiles instead.
    
    - Create dedicated AuiSelectDeviceProfiles component for proper device profile selection
    - Refactor AuiNewPbxDevice to use the new device profile select component
    - Add getDeviceProfile action to customers store for data retrieval
    
    This fixes the Cypress test failures and ensures correct device profile
    selection in the PBX device forms.
    
    Change-Id: Ib398e4269e4a8cb95cec01ddc673dc19ed2e1bee
    (cherry picked from commit 0188fb744ff6a455f185f19d50aa5d73b1ad9876)

---
+++ src/components/AuiSelectDeviceProfiles.vue
@@ -0,0 +1,41 @@
+<template>
+    <aui-select-lazy
+        :label="$t('Device Profiles')"
+        icon="fas fa-fax"
+        clearable
+        dense
+        store-generator-name="selectLazy/deviceProfilesList"
+        :initial-option="initialOption"
+        :store-action-params="{
+            expand: 'device_id'
+        }"
+        :load-initially="false"
+        v-bind="$attrs"
+    >
+        <template
+            v-for="(_, slotName) of $slots"
+            #[slotName]="scope"
+        >
+            <slot
+                :name="slotName"
+                v-bind="scope"
+            />
+        </template>
+    </aui-select-lazy>
+</template>
+
+<script>
+import AuiSelectLazy from 'components/input/AuiSelectLazy'
+export default {
+    name: 'AuiSelectDeviceProfiles',
+    components: {
+        AuiSelectLazy
+    },
+    props: {
+        initialOption: {
+            type: Object,
+            default: null
+        }
+    }
+}
+</script>
