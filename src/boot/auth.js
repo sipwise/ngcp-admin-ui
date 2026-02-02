8bfb1d5a0d2b7d56e8553972fc5da8b4b3506a61
Debora Crescenzo <dcrescenzo@sipwise.com>
2026-02-02 11:59:25 +0000

    MT#64264 Password reset page bugfix
    Users were not able to access the password reset page
    because the router.beforeEach logic treated the
    recover-password path like any other protected route,
    redirecting users to login.
    This commit adds logic to handle the recover-password path:
    - The page is accessible only if ?token= exists in the URL.
    - The token is validated when sending data to the backend (as before).
    
    Change-Id: I2f5e3020b13806c4e30e5f84e5cc4943e60472b9
    (cherry picked from commit bcbb5b20eb1a518af4fa0eee19b424a85eac01c0)
    (cherry picked from commit 71a5af382b835c978afa260a62514af1a3e7a5b9)

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -9,6 +9,7 @@ import {
     PATH_ERROR_403,
     PATH_ERROR_404,
     PATH_LOGIN,
+    PATH_RECOVER_PASSWORD,
     QUERY_PARAM_AUTH_V1
 } from 'src/router/common'
 const publicPaths = [
@@ -21,6 +22,10 @@ const publicPaths = [
 export default async ({ router, redirect }) => {
     router.beforeEach((to, from, next) => {
         if (!hasJwt() && !publicPaths.includes(to.path)) {
+            if (to.path === PATH_RECOVER_PASSWORD && to.query.token) {
+                next()
+                return
+            }
             if (from?.path === PATH_LOGIN) {
                 next(false)
             } else {
