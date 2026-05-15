f14ab16ac9dbe92493bebdda9a9ace03ac7f3753
Debora Crescenzo <dcrescenzo@sipwise.com>
2026-05-15 14:25:17 +0100

    MT#65112 AuiLazySelect bugfix
    Resellers, Contracts, Contry, Time Sets and Timezone
    dropdown were only listing the fist 10 resources, the
    request for the following page never happened.
    Actions ignored the pagination parameters,
    chenged the use of Math.trunc() with Math.ceil()
    Mutations were always replacing the data instead
    of accumulating.
    
    Change-Id: I5d2edbd30d05de26883cebdaa26fbf6a1421f6bf
    (cherry picked from commit f6d499de45c525301148a521d2cd7186ea2902e3)

--- src/store/country/mutations.js
+++ src/store/country/mutations.js
@@ -1,3 +1,10 @@
-export function countries (state, countries) {
-    state.countries = countries
+export function countries (state, payload) {
+    const countries = payload.countries
+    const page = payload.page ?? 0
+
+    if (page > 1) {
+        state.countries = [...state.countries, ...countries]
+    } else {
+        state.countries = countries
+    }
 }
--- src/store/timezone/actions.js
+++ src/store/timezone/actions.js
@@ -10,13 +10,22 @@ export async function fetchTimezones ({ commit }, options) {
 }
 
 export async function filterTimezones ({ commit, dispatch }, filter) {
+    const filterObj = (typeof filter === 'object') ? filter : { filter }
+    const page = filterObj.page ?? 1
+    const rowsPerPage = filterObj.rows ?? 10
+
     const timezones = await dispatch('fetchTimezones', {
-        filter: (typeof filter === 'object') ? filter?.filter : filter,
+        filter: filterObj.filter,
         pagination: {
             sortBy: 'id',
             descending: false,
+            page: page === 0 ? 1 : page,
+            rowsPerPage,
             rowsNumber: null
         }
     })
-    commit('timezones', _.get(timezones, 'aaData', []))
+    commit('timezones', {
+        timezones: _.get(timezones, 'aaData', []),
+        page
+    })
 }
